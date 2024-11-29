c1de2472766d07383913f6b68461ab933d6f08f0
GermanoC <105460981+GermanoCesarSS@users.noreply.github.com>
2024-11-29 16:51:59 -0300

    Ambiente pronto

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,10 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from tcc_madr.app import app
-
-client = TestClient(app)
-
-
-def test_ola_mundo():
-    client = TestClient(app)
-
-    response = client.get('/')
+def test_ola_mundo(client):
+    response = client.get(
+        '/',
+    )
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo!'}
+    assert response.json() == {'mensagem': 'Olá Mundo!'}
